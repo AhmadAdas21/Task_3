56ae6a1b5712ea0f71f5a8ae66bd48ad157c71b7
AhmadAdas21 <ahmadnajiadas20@gmail.com>
2026-09-21 21:23:54 +0300

    task 3 is ready

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -16,54 +16,86 @@ namespace Task_3
         List<order>orders=new List<order>();
 
         static void Main(string[] args)
-
         {
             bool ok = true;
-            int c;
+
+            services service = new services();
+
             while (ok)
             {
                 Console.WriteLine("************************");
                 Console.WriteLine("select one of our features");
-                Console.WriteLine("1 make an oreder");
+                Console.WriteLine("1 make an order");
                 Console.WriteLine("2 view all orders");
                 Console.WriteLine("3 search for an order");
-                Console.WriteLine("4  show orders above the amount");
-                Console.WriteLine("show completed orders");
+                Console.WriteLine("4 show orders above the amount");
+                Console.WriteLine("5 show completed orders");
                 Console.WriteLine("6 sort orders by amount");
                 Console.WriteLine("7 sort orders by date");
-                Console.WriteLine("8 calculate total amount of the order");
+                Console.WriteLine("8 calculate total amount of the orders");
                 Console.WriteLine("9 group orders by customers");
                 Console.WriteLine("10 exit");
+
                 int d;
-                if (int.TryParse(args[0], out d))
+
+                if (!int.TryParse(Console.ReadLine(), out d))
                 {
-                    Console.WriteLine("you choice has received");
+                    Console.WriteLine("enter a valid choice between 1 and 10");
+                    continue;
                 }
-                else
-                {
-                    Console.WriteLine("enter a valid choice between 1 and 9");
-                }
+
                 switch (d)
                 {
-                    case 1:make_order();break;
-                    case 2:view_all() break;
-                    case 3: search_order();break;
-                    case 4: show_order();break;
-                    case 5: completed_orders();break;
-                    case 6:sort_order_by_amount();break;
-                    case 7:sort_order_by_date();break;
-                    case 8:total_amount();break;
-                    case 9:group_orders_by_customers();break;
-                    case 10:Console.WriteLine("thank you for using our system");ok = false;break;
+                    case 1:
+                        service.make_order();
+                        break;
 
+                    case 2:
+                        service.view_all();
+                        break;
 
+                    case 3:
+                        service.search_order();
+                        break;
+
+                    case 4:
+                        service.show_order();
+                        break;
+
+                    case 5:
+                        service.completed_orders();
+                        break;
+
+                    case 6:
+                        service.sort_order_by_amount();
+                        break;
+
+                    case 7:
+                        service.sort_order_by_date();
+                        break;
+
+                    case 8:
+                        service.total_amount();
+                        break;
+
+                    case 9:
+                        service.group_orders_by_customers();
+                        break;
+
+                    case 10:
+                        Console.WriteLine("thank you for using our system");
+                        ok = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("choose a number between 1 and 10");
+                        break;
                 }
             }
-            
         }
 
 
 
-       
+
     }
 }
